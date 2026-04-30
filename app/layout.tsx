25cd7134edf39eb287e8a4de74c900fff725d0a7
Adam <lewison97@gmail.com>
2026-04-30 13:39:01 +0100

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export const metadata: Metadata = {
     ],
     shortcut: "/favicon/favicon.ico",
     apple: {
-      url: "/favicon/apple-touch-icon.png",
+      url: "favicon/apple-touch-icon.png",
       sizes: "180x180",
       type: "image/png",
     },
